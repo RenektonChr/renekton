939c429e098eed946d43d3ce741002b92f4f2736
Renekton <1054346513@qq.com>
2025-05-18 15:57:21 +0800

    Update app.module.ts to include buggy components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BuggyModule } from './buggy.module';
+import { BuggyController } from './buggy.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [BuggyModule],
+  controllers: [AppController, BuggyController],
   providers: [AppService],
 })
 export class AppModule {}
